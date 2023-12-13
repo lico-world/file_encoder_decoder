ce3990cc39bb898f0bbb13ebaa82fd117e22c64c
lico <107700554+lico-world@users.noreply.github.com>
2023-12-13 18:37:59 +0200

    Time comparison using the recursive Fibonacci function

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import ctypes
 import os
+import time
+
 
 lib = ctypes.CDLL(os.path.abspath('dlls/encoder_decoder.dll'))
-lib.call(17)
+
+
+def fibo(n):
+    if n == 0 or n == 1:
+        return n
+    return fibo(n-1) + fibo(n-2)
+
+
+START_C = time.perf_counter()
+lib.fibo(37)
+END_C = time.perf_counter()
+
+START_PYTHON = time.perf_counter()
+fibo(37)
+END_PYTHON = time.perf_counter()
+
+TIME_C = END_C-START_C
+TIME_PYTHON = END_PYTHON-START_PYTHON
+print('Python\t\t: ', TIME_PYTHON, 's\nC\t\t\t: ', TIME_C, 's\nDifference\t: ',
+      100*(TIME_PYTHON-TIME_C)/TIME_PYTHON, '%')
--- src/c/encoder_decoder.c
+++ src/c/encoder_decoder.c
@@ -4,3 +4,9 @@ void call(int nb)
 {
     printf("Called with half of %d\n", nb*2);
 }
+
+int fibo(int n)
+{
+    if(n == 0 || n == 1) return n;
+    return fibo(n-1) + fibo(n-2);
+}
