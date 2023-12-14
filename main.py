fa43d387c0565ff0f98b3c145c3ab5bdccc7dc7b
lico <107700554+lico-world@users.noreply.github.com>
2023-12-14 12:09:44 +0200

    Simple text message encryption/decryption

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import ctypes
 import os
 
+
 lib = ctypes.CDLL(os.path.abspath('dlls/encoder_decoder.dll'))
-lib.call(17)
+lib.encode.restype = ctypes.c_char_p
+lib.decode.restype = ctypes.c_char_p
+
+msg = b'hello world how are you'
+key = b'encryption key'
+print('Initial message\t: ', msg.decode('utf-8'))
+
+encoded = lib.encode(msg, key, int(len(msg)), int(len(key)))
+print('Message encoded\t: ', encoded.decode('utf-8'))
+
+decoded = lib.decode(encoded, key, int(len(msg)), int(len(key)))
+print('Message decoded\t: ', decoded.decode('utf-8'))
--- src/c/encoder_decoder.c
+++ src/c/encoder_decoder.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void call(int nb)
+const char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890";
+const size_t alphabetLength = strlen(alphabet);
+
+static int findInAlphabet(char c)
 {
-    printf("Called with half of %d\n", nb*2);
+    for(int idx = 0; idx < alphabetLength ; idx++)
+    {
+        if(c == alphabet[idx]) return idx;
+    }
+    return -1;
+}
+
+char* encode(char* MSG, char* KEY, int MSG_LENGTH, int KEY_LENGTH)
+{
+    char* encoded = (char*)malloc(MSG_LENGTH * sizeof(char));
+    for(int idx=0 ; idx<MSG_LENGTH ; idx++)
+    {
+        int newCharIndex = findInAlphabet(MSG[idx]) + findInAlphabet(KEY[idx % KEY_LENGTH]);
+        encoded[idx] = alphabet[newCharIndex % alphabetLength];  // Avoid OOB
+    }
+    return encoded;
+}
+
+char* decode(char* MSG, char* KEY, int MSG_LENGTH, int KEY_LENGTH)
+{
+    char* decoded = (char*)malloc(MSG_LENGTH * sizeof(char));
+    for(int idx=0 ; idx<MSG_LENGTH ; idx++)
+    {
+        int newCharIndex = findInAlphabet(MSG[idx]) - findInAlphabet(KEY[idx % KEY_LENGTH]);
+        decoded[idx] = alphabet[(newCharIndex + alphabetLength) % alphabetLength];  // '+ alphabetLength' to avoid negative indexes
+    }
+    return decoded;
 }
